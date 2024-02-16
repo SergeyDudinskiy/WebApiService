57a01a320b3d334b301bbf3a2b00a084d30665c1
DudinskiySergey <2sery@mail.ru>
2024-02-16 14:53:50 +0300

    Added delete function for bd records

--- WebApiService/Mutaties/Mutation.cs
+++ WebApiService/Mutaties/Mutation.cs
@@ -5,7 +5,7 @@ namespace WebApiService.Mutaties
 {
     public class Mutation
     {
-        public async Task<Book> Book([Service] DataDbContext dbContext, string title, int pages, string author, int chapters)
+        public async Task<Book> AddBook([Service] DataDbContext dbContext, string title, int pages, string author, int chapters)
         {
             var book = new Book
             {
@@ -18,9 +18,22 @@ namespace WebApiService.Mutaties
             dbContext.Books.Add(book);
             await dbContext.SaveChangesAsync();
             return book;
-        }       
+        }
 
-        public async Task<Reader> Reader([Service] DataDbContext dbContext, string firstname, string lastname)
+        public async Task<Book> DeleteBook([Service] DataDbContext dbContext, int id)
+        {
+            var book = dbContext.Books.FirstOrDefault(x => x.Id == id);
+            
+            if (book != null)
+            {
+                dbContext.Books.Remove(book);
+            }
+
+            await dbContext.SaveChangesAsync();
+            return null;
+        }
+
+        public async Task<Reader> AddReader([Service] DataDbContext dbContext, string firstname, string lastname)
         {
             var reader = new Reader
             {
@@ -31,6 +44,19 @@ namespace WebApiService.Mutaties
             dbContext.Readers.Add(reader);
             await dbContext.SaveChangesAsync();
             return reader;
+        }
+
+        public async Task<Reader> DeleteReader([Service] DataDbContext dbContext, int id)
+        {
+            var reader = dbContext.Readers.FirstOrDefault(x => x.Id == id);
+
+            if (reader != null)
+            {
+                dbContext.Readers.Remove(reader);
+            }
+
+            await dbContext.SaveChangesAsync();
+            return null;
         }
 
         public async Task<BooksInReaders> BooksInReaders([Service] DataDbContext dbContext, int readerId, int bookId)
@@ -44,6 +70,19 @@ namespace WebApiService.Mutaties
             dbContext.BooksInReaders.Add(booksInReaders);
             await dbContext.SaveChangesAsync();
             return booksInReaders;
+        }
+
+        public async Task<BooksInReaders> DeleteBooksInReaders([Service] DataDbContext dbContext, int id)
+        {
+            var booksInReaders = dbContext.BooksInReaders.FirstOrDefault(x => x.Id == id);
+
+            if (booksInReaders != null)
+            {
+                dbContext.BooksInReaders.Remove(booksInReaders);
+            }
+
+            await dbContext.SaveChangesAsync();
+            return null;
         }
     }
 }
